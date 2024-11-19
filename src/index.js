a7f9c6bfdebade25452cf47d867ae5e2eaffe845
Luciana-Gassi <luciana.gassi@gmail.com>
2024-11-19 21:00:36 +0100

    Aggiunta della cartella build per la distribuzione

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import i18n from "./i18n"; // Importa la configurazione di i18next
+import { I18nextProvider } from "react-i18next"; // Importa il provider di i18next
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<I18nextProvider i18n={i18n}>
+			{/* {" "} */}
+			{/* Avvolgi l'app con il provider */}
+			<App />
+		</I18nextProvider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
